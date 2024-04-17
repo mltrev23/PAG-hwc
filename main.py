8ae18684ce2b4ad4cf7b7e14670f4959a4a9f26a
mltrev23 <trevor.dev23@gmail.com>
2024-04-17 12:20:37 -0500

    image generating

---
+++ main.py
@@ -0,0 +1,24 @@
+from diffusers import StableDiffusionPipeline
+
+pipe = StableDiffusionPipeline.from_pretrained(
+    "runwayml/stable-diffusion-v1-5",
+    custom_pipeline="hyoungwoncho/sd_perturbed_attention_guidance",
+    torch_dtype=torch.float16,
+    safety_checker=None
+)
+
+device="cuda"
+pipe = pipe.to(device)
+
+prompts = ["a corgi"]
+
+output = pipe(
+        prompts,
+        width=512,
+        height=512,
+        num_inference_steps=50,
+        guidance_scale=0.0,
+        pag_scale=5.0,
+        pag_applied_layers_index=['m0']
+    ).images
+output.save()
